9214d7c18a908e475ad8f5fa4d18ad3f27df090f
Tristan Genevet <tristan@unity3d.com>
2020-01-15 18:50:12 +0100

    Keep a cache of vfx dependencies to speedup compilation with dependencies.

--- com.unity.visualeffectgraph/Editor/Models/VFXGraph.cs
+++ com.unity.visualeffectgraph/Editor/Models/VFXGraph.cs
@@ -648,7 +648,7 @@ namespace UnityEditor.VFX
             }
         }
 
-        void SubgraphDirty(VisualEffectObject subgraph)
+        public void SubgraphDirty(VisualEffectObject subgraph)
         {
             if (m_SubgraphDependencies != null && m_SubgraphDependencies.Contains(subgraph))
             {
@@ -665,7 +665,7 @@ namespace UnityEditor.VFX
             Profiler.EndSample();
         }
 
-        IEnumerable<VFXGraph> GetAllGraphs<T>() where T : VisualEffectObject
+        public static IEnumerable<VFXGraph> GetAllGraphs<T>() where T : VisualEffectObject
         {
             var guids = AssetDatabase.FindAssets("t:" + typeof(T).Name);
 
@@ -766,10 +766,7 @@ namespace UnityEditor.VFX
                 if (m_DependentDirty)
                 {
                     var obj = GetResource().visualEffectObject;
-                    foreach (var graph in GetAllGraphs<VisualEffectAsset>())
-                    {
-                        graph.SubgraphDirty(obj);
-                    }
+                    VFXExternalShaderProcessor.RecompileDependencies(obj);
                     m_DependentDirty = false;
                 }
             }
