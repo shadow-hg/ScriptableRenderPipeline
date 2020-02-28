e942421979af17f5d39760f08571d9e6234afde0
Elizabeth LeGros <elizabeth.legros@unity3d.com>
2020-02-28 15:07:24 -0600

    #include cleanup

--- com.unity.shadergraph/Editor/Data/Graphs/GraphConcretization.cs
+++ com.unity.shadergraph/Editor/Data/Graphs/GraphConcretization.cs
@@ -1,13 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using UnityEngine;
-using UnityEditor.Graphing;
-using UnityEditor.Graphing.Util;
-using UnityEditor.Rendering;
-using UnityEditor.ShaderGraph.Internal;
-using Edge = UnityEditor.Graphing.Edge;
 
 namespace UnityEditor.ShaderGraph
 {
